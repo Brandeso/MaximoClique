a40e3fefef29a15364bee6a728bfaa32cf5f0a3b
Bruno Amezcua <bruno.amezcua@bwr.mx>
2020-07-08 01:00:29 -0500

    Se crean las screens y los componentes

--- MaximoClique/src/app/app.module.ts
+++ MaximoClique/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from 'src/screens/home/home.component';
-import { AboutComponent } from 'src/screens/about/about.component';
+
+// Screens
+import { HomeScreen } from 'src/screens/home/home.screen';
+import { AboutScreen } from 'src/screens/about/about.screen';
+import { AlgorithmScreen } from 'src/screens/algorithm/algorithm.screen';
+
+// Components
+import { InfoComponent } from 'src/components/about/about.component';
+import { InputComponent } from 'src/components/input/input.component';
+import { ResultsComponent } from 'src/components/results/results.component';
+import { VideoComponent } from 'src/components/video/video.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent
+    HomeScreen,
+    AboutScreen,
+    AlgorithmScreen,
+    InfoComponent,
+    InputComponent,
+    ResultsComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule,
---
+++ MaximoClique/src/screens/home/home.screen.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.screen.html',
+  styleUrls: ['./home.screen.scss']
+})
+export class HomeScreen {
+
+  constructor() { }
+
+}
