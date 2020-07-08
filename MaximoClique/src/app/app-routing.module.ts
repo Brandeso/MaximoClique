3fffc4951d4f2c04adcd90fee3f585f88f32afdf
Bruno Amezcua <bruno.amezcua@bwr.mx>
2020-07-08 01:03:19 -0500

    Se configura el routing de la app

--- MaximoClique/src/app/app-routing.module.ts
+++ MaximoClique/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeScreen } from 'src/screens/home/home.screen';
+import { AlgorithmScreen } from 'src/screens/algorithm/algorithm.screen';
+import { AboutScreen } from 'src/screens/about/about.screen';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: AboutScreen},
+  { path: 'algorithm', component: AlgorithmScreen}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
