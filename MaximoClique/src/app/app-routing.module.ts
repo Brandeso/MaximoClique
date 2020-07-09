e7dc1a730995a78db2809baa1451fcc08f93b26e
luisgmdz <dekrskate@gmail.com>
2020-07-08 23:41:52 -0500

    correcion de comentarios en rutas

--- MaximoClique/src/app/app-routing.module.ts
+++ MaximoClique/src/app/app-routing.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeScreen } from 'src/screens/home/home.screen';
 import { AlgorithmScreen } from 'src/screens/algorithm/algorithm.screen';
 import { AboutScreen } from 'src/screens/about/about.screen';
-import { ResultsComponent } from 'src/components/results/results.component';
-import { InputComponent } from 'src/components/input/input.component';
+
 
 
 const routes: Routes = [
-  //{ path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: AboutScreen},
-  { path: 'algorithm', component: AlgorithmScreen},
-  { path: 'input', component: InputComponent},
-  { path: 'results', component: ResultsComponent}
-
+  { path: 'algorithm', component: AlgorithmScreen}
 ];
 
 @NgModule({
--- MaximoClique/src/components/results/results.component.ts
+++ MaximoClique/src/components/results/results.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from "@angular/core";
-import { enableDebugTools } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-results',
